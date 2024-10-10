6dbc956b940096bb49c79b274e187b77db35e2d1
Jonathan Chemla <jos.chemla@gmail.com>
2024-10-10 17:20:42 +0200

    Add Google Earth Web Historical Imagery link

--- src/links-section.tsx
+++ src/links-section.tsx
@@ -13,33 +13,20 @@ function LinksSection(props: { mapRef: any }) {
       {" "}
       Useful:{" "}
       <Link
-        href="https://google.com/intl/fr/earth/versions/#earth-pro"
-        target={"_blank"}
-      >
-        Google Earth Pro Desktop
-      </Link>
-      {" (with Historical imagery or "}
-      <Link
         href={`https://earth.google.com/web/@${center?.lat},${center?.lng},0a,${((38000 * 4096) / Math.pow(2, zoom)) *
           Math.cos((center?.lat * Math.PI) / 180)
-          }d,35y,0h,0t,0r`}
+          }d,35y,0h,0t,0r/data=CgwqBggBEgAYAUICCAE6AwoBMEoNCP___________wEQAA`}
         target={"_blank"}
       >
-        Web
+        Google Earth Web
       </Link>
-      {") | ESRI "}
+      {" Historical + Timelapse"}
+      {" | ESRI "}
       <Link
         href={`https://livingatlas.arcgis.com/wayback/#active=37890&ext=${bounds?.getWest()},${bounds?.getSouth()},${bounds?.getEast()},${bounds?.getNorth()}&localChangesOnly=true`}
         target={"_blank"}
       >
         Imagery Wayback Machine
-      </Link>
-      {" | and "}
-      <Link
-        href={`https://earthengine.google.com/timelapse#v=${center?.lat},${center?.lng},${zoom},latLng&t=0.03&ps=50&bt=19840101&et=20201231&startDwell=0&endDwell=0`}
-        target={"_blank"}
-      >
-        Google Timelapse
       </Link>
       {" | "}
       <Link href={`https://qms.nextgis.com/#`}>NextGIS QMS</Link>
@@ -49,6 +36,21 @@ function LinksSection(props: { mapRef: any }) {
         target={"_blank"}
       >
         BBBike MapCompare
+      </Link>
+      {" | "}
+      {" | Older: Google "}
+      <Link
+        href={`https://earthengine.google.com/timelapse#v=${center?.lat},${center?.lng},${zoom},latLng&t=0.03&ps=50&bt=19840101&et=20201231&startDwell=0&endDwell=0`}
+        target={"_blank"}
+      >
+        Timelapse
+      </Link>
+      {" & "}
+      <Link
+        href="https://google.com/intl/fr/earth/versions/#earth-pro"
+        target={"_blank"}
+      >
+        Earth Pro Desktop
       </Link>
       {" | "}
       <Link
